7cf8a8237f0b2b20a5527c2f8179f636122eb297
Cass Fridkin <cass@swag.lgbt>
2024-11-21 22:22:57 -0700

    Extract to user-local dir

--- scripts/build-pages.sh
+++ scripts/build-pages.sh
@@ -12,7 +12,7 @@ echo "Downloading \`rage\` from GitHub to $TEMP_DEB"
 wget -O "$TEMP_DEB" "https://github.com/str4d/rage/releases/download/v$RAGE_VERSION/rage_$RAGE_VERSION-1_amd64.deb"
 
 echo "Installing \`rage\`"
-sudo dpkg -i "$TEMP_DEB"
+sudo dpkg -x "$TEMP_DEB" "$HOME"/.local/share
 
 echo "Cleaning up $TEMP_DEB"
 rm -f "$TEMP_DEB"
