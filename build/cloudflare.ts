c3f3865eaa774038da316d62d870f121a46e3874
Cass Fridkin <cass@swag.lgbt>
2024-11-22 21:48:58 -0700

    Move custom build functionality into astro integrations

--- build/cloudflare.ts
+++ build/cloudflare.ts
@@ -1,29 +1,78 @@
 import process from "node:process";
 
-export function cfPagesBuildMetadata() {
-  const { CF_PAGES, CF_PAGES_COMMIT_SHA, CF_PAGES_BRANCH, CF_PAGES_URL } =
-    process.env;
+import type { AstroIntegration } from "astro";
+import { envField } from "astro/config";
 
-  switch (CF_PAGES) {
-    case undefined:
-      return;
-    case "1":
-      break;
-    default:
-      throw new Error(`CF_PAGES was set to unexpected value: ${CF_PAGES}`);
-  }
+export function cfPagesEnvVars(): AstroIntegration {
+  return {
+    name: "cloudflare pages vars",
+    hooks: {
+      "astro:config:setup": ({ updateConfig, logger }) => {
+        updateConfig({
+          // TODO: change this to "static" once @astrojs/cloudflare catches up to astro 5
+          output: "server",
+          env: {
+            schema: {
+              // Variables set by the Cloudflare Pages build system
+              CF_PAGES: envField.number({
+                access: "public",
+                context: "client",
+                default: 0,
+                optional: true,
+              }),
+              CF_PAGES_COMMIT_SHA: envField.string({
+                access: "public",
+                context: "client",
+                optional: true,
+              }),
+              CF_PAGES_BRANCH: envField.string({
+                access: "public",
+                context: "client",
+                optional: true,
+              }),
+              CF_PAGES_URL: envField.string({
+                access: "public",
+                context: "client",
+                optional: true,
+              }),
+            },
+          },
+        });
 
-  if (CF_PAGES_COMMIT_SHA === undefined || CF_PAGES_COMMIT_SHA.length === 0) {
-    throw new Error(`CF_PAGES was set, but CF_PAGES_COMMIT_SHA is undefined`);
-  }
+        const { CF_PAGES, CF_PAGES_COMMIT_SHA, CF_PAGES_BRANCH, CF_PAGES_URL } =
+          process.env;
 
-  if (CF_PAGES_BRANCH === undefined || CF_PAGES_BRANCH.length === 0) {
-    throw new Error(`CF_PAGES was set, but CF_PAGES_BRANCH is undefined`);
-  }
+        switch (CF_PAGES) {
+          case undefined:
+            logger.warn("Not setting `site` since CF_PAGES is unset.");
+            return;
+          case "1":
+            break;
+          default:
+            throw new Error(
+              `CF_PAGES was set to unexpected value: ${CF_PAGES}`
+            );
+        }
 
-  if (CF_PAGES_URL === undefined || CF_PAGES_URL.length === 0) {
-    throw new Error(`CF_PAGES was set, but CF_PAGES_URL is undefined`);
-  }
+        if (
+          CF_PAGES_COMMIT_SHA === undefined ||
+          CF_PAGES_COMMIT_SHA.length === 0
+        ) {
+          throw new Error(
+            `CF_PAGES was set, but CF_PAGES_COMMIT_SHA is undefined`
+          );
+        }
 
-  return { CF_PAGES_COMMIT_SHA, CF_PAGES_BRANCH, CF_PAGES_URL };
+        if (CF_PAGES_BRANCH === undefined || CF_PAGES_BRANCH.length === 0) {
+          throw new Error(`CF_PAGES was set, but CF_PAGES_BRANCH is undefined`);
+        }
+
+        if (CF_PAGES_URL === undefined || CF_PAGES_URL.length === 0) {
+          throw new Error(`CF_PAGES was set, but CF_PAGES_URL is undefined`);
+        }
+
+        updateConfig({ site: CF_PAGES_URL });
+      },
+    },
+  };
 }
